d256838c8d5fce791aacbd2f5d410de85ddd92e6
Leather-Face619 <rahul852369hhhhh@gmail.com>
2024-05-25 19:25:22 +0530

    2nd commit

--- part2.js
+++ part2.js
@@ -13,4 +13,15 @@ console.log(verifyemail("hag@hagasdsad.com"))
 console.log(verifyemail("rahul852369@gmail.com"));
 console.log(verifyemail("rahul852369@gmail.in"));
 console.log(verifyemail("rahul852369@gmail.ai"));
-console.log(verifyemail("rahul852369@gma23il.com"));
+console.log(verifyemail("rahul852sd369@gma23il.com"));
+// console.table("hahahahah");
+
+var date = (value)=>{
+    // var date = /\d{2}\/\d{2}\/\d{4}/
+    var date = /^([12][0-9]|[1-9]|0[1-9])\/([0-9]|1[0-2])\/((19|20)[0-9]|1[0-9])[1-9]/
+    return date.test(value)
+}
+
+console.log(date("11/4/2016"))
+console.log(date("12/12/1920"))
+console.log(date("5/13/2024"))
